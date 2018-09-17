68cb7b77ff829cd496f9c4447f98de55975010b9
nc-huete <37090961+nc-huete@users.noreply.github.com>
2018-09-17 01:04:34 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,55 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/producto/',
+    	url: 'producto.html',
+    	name: 'producto',
+  		},
+		{
+		path: '/about/',
+    	url: 'about.html',
+    	name: 'about',
+  		},
+		{
+		path: '/shops/',
+    	url: 'shops.html',
+    	name: 'shops',
+  		},
+		{
+		path: '/shops-tgu/',
+    	url: 'shops-tgu.html',
+    	name: 'shops-tgu',
+  		},
+		{
+		path: '/shops-sps/',
+    	url: 'shops-sps.html',
+    	name: 'shops-sps',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
